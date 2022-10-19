ca7dd8a8e8d022155935419fedc31c85d169a3b7
Jeremy Buist <jbuistjbuist@gmail.com>
2022-10-19 18:44:17 -0400

    airbnb queries 1 exercise

---
+++ 1_queries/4_visits_per_city.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT properties.city, COUNT(reservations) AS total_reservations
+
+FROM properties
+JOIN reservations
+  ON properties.id = property_id
+
+GROUP BY properties.city
+ORDER BY total_reservations DESC;
+
